d2b7b93e828135dc76278380e4337dcf12e1c1d5
Wuinsee <5wqbmbmv6f@privaterelay.appleid.com>
2026-02-15 19:13:59 -0600

    Create laboratorio 3.cs

---
+++ laboratorio 3.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+using System;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Programa hecho por: Wuin See Salguero");
+
+        string nombre;
+        string curso;
+
+        Console.WriteLine();
+
+        Console.Write("Escriba su nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("Escriba su curso: ");
+        curso = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Bienvenido " + nombre + " del curso: " + curso);
+
+        Console.WriteLine();
+        Console.Write("Presione una tecla para salir");
+        Console.ReadKey();
+    }
+}
+
+
+
